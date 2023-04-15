3f46a53d83867029e531765e8103576bdd8f7d52
Julio Leon <juliopena143@icloud.com>
2023-04-15 14:24:55 -0600

    bug fix

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,11 +3,11 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
-    // "./pages/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
+  // ...
   plugins: [],
 }
 
